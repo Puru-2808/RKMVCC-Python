ce59dde2573fc419319ccc4f3d307a60ad692912
Puru-2808 <purusottamrooj2021@gmail.com>
2026-04-11 04:03:24 +0530

    adding

--- Fourier Transform.py
+++ Fourier Transform.py
@@ -3,30 +3,32 @@ import numpy as np
 from scipy import integrate
 
 # previous function
-xi = -100
-xf = 100
+xi = -20
+xf = 20
 n = 1001
 h = (xf-xi)/(n-1)
 
 xx=[]
 yy=[]
+XX1=[]
+yy1=[]
 
 for i in range (n):
     x=xi+i*h
     xx.append(x)
-    y= np.exp(-x**2)
+    y = np.sin(3*x) + 0.6*np.sin(7*x) + 0.3*np.sin(12*x)
     yy.append(y)
 
 # transformed fuction
-ti=xi
-tf=xf
-
+ti=-20
+tf=20
+h1=(tf-ti)/(n-1)
 tt=[]
 aa=[]
 
 
 for i in range(n):
-    t=ti+i*h
+    t=ti+i*h1
     tt.append(t)
     
 
@@ -36,9 +38,18 @@ for i in range (len(tt)):
     for k in range(n):
         f = np.exp(-1j*tt[i]*xx[k])*yy[k]
         ff.append(f)
-    a=integrate.simpson(ff,x=xx)/((2**0.5)*np.pi)
+    a=(integrate.simpson(ff,x=xx))/((2*np.pi)**0.5)
     aa.append(a)
 
-plt.plot(xx,yy,'-k')
-plt.plot(tt,np.real(aa),'-r')
+plt.figure(figsize=(10,4))
+
+plt.subplot(1,2,1)
+plt.plot(xx, yy, 'k')
+plt.title("Time-domain signal")
+
+plt.subplot(1,2,2)
+plt.plot(tt, np.abs(aa), 'r')
+plt.title("Frequency spectrum")
+
+plt.tight_layout()
 plt.show()
